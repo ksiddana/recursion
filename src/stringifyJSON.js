613e718f474dc78a2c95645806564b72e9502d87
Karun Siddana <ksiddana@gmail.com>
2015-11-16 21:16:22 -0800

    fixed the tab spaces

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -19,48 +19,47 @@ var stringifyJSON = function(obj) {
   				return JSONstring;
   	} else if (Array.isArray(obj)) {
 					
-					var result = "";
+		var result = "";
 
-					if (obj.length === 0) {
-							return '[' + obj + ']';
-					} else if (obj.length === 1 && typeof obj[0] === "number") {
-							return '[' + obj + ']';
-					} else if (obj.length === 1 && typeof obj[0] === "string") {
-							return '[' + '"' + obj + '"' + ']';
-					}
+		if (obj.length === 0) {
+				return '[' + obj + ']';
+		} else if (obj.length === 1 && typeof obj[0] === "number") {
+				return '[' + obj + ']';
+		} else if (obj.length === 1 && typeof obj[0] === "string") {
+				return '[' + '"' + obj + '"' + ']';
+		}
 
-					for (var i = 0; i < obj.length; i++) {
-							var JSONstring;
-							console.log("-----------------");
-							console.log(obj[i]);
-							console.log(typeof obj[i]);
-							console.log("Object Length: ", obj.length);
+		for (var i = 0; i < obj.length; i++) {
+				var JSONstring;
+				/*
+				console.log("-----------------");
+				console.log(obj[i]);
+				console.log(typeof obj[i]);
+				console.log("Object Length: ", obj.length);
+				*/
+				if (typeof obj[i] === "number" && obj.length !== 1) {
+						JSONstring = obj[i];
+				} else if (typeof obj[i] === "string" && obj.length !== 1) {
+						JSONstring = '"' + obj[i] + '"';
+				} else if (typeof Array.isArray(obj[i])) {
+						JSONstring = stringifyJSON(obj[i]);
+				}
 
-							if (typeof obj[i] === "number" && obj.length !== 1) {
-									console.log("Reached here 1.")
-									JSONstring = obj[i];
-							} else if (typeof obj[i] === "string" && obj.length !== 1) {
-									console.log("Reached here 2.");
-									JSONstring = '"' + obj[i] + '"';
-							} else if (typeof Array.isArray(obj[i])) {
-									JSONstring = stringifyJSON(obj[i]);
-							}
 
-							
-							result += JSONstring + ",";	
-							
+				result += JSONstring + ",";	
 
-							/*					
-							If you declare result as an [] above then you have to use this convention
-							Also there is no need for commas ',' if you do this method.
-							result.push(JSONstring);
-							*/
-							console.log("Result: ", result);
-					}
 
-						result = result.substring(0,result.length-1);
-						console.log("Final Output: ", result);
-						return '[' + result + ']';
+				/*					
+				If you declare result as an [] above then you have to use this convention
+				Also there is no need for commas ',' if you do this method.
+				result.push(JSONstring);
+				*/
+//							console.log("Result: ", result);
+		}
+
+			result = result.substring(0,result.length-1);
+//						console.log("Final Output: ", result);
+			return '[' + result + ']';
 				
 //      _.each(obj, function(item){
 //        console.log(item);
@@ -75,34 +74,34 @@ var stringifyJSON = function(obj) {
       //     }
       // }
     }
-			else {
+	else {
 				
-				console.log("**********************");
-				console.log(obj);	
-				var result = "";
-				
-				for (var key in obj) {
-					var JSONstring = '';
-					
-					console.log(typeof obj[key]);
-					
-					if (obj[key] === true || obj[key] === false || obj[key] === null) {
-							JSONstring = '"' + key + '"' + ":" + obj[key];
-					} else if (typeof obj[key] === "string" || typeof obj[key] === "number") {
-							JSONstring = '"' + key + '"' + ":" + '"' + obj[key] + '"';	
-					} else if (obj.hasOwnProperty('functions') || obj.hasOwnProperty('undefined')) {
-							return '{}';	
-					} else {
-							JSONstring = '"' + key + '"' + ":" + stringifyJSON(obj[key]);
-					}
-					
-					result += JSONstring + ",";
-					console.log("Result: ", result);
-				}
-				
-				result = result.substring(0, result.length-1);
-				console.log("Final Output: ", result);
-				return '{' + result + '}';
-					
+//		console.log("**********************");
+//		console.log(obj);	
+		var result = "";
+
+		for (var key in obj) {
+			var JSONstring = '';
+
+//					console.log(typeof obj[key]);
+
+			if (obj[key] === true || obj[key] === false || obj[key] === null) {
+					JSONstring = '"' + key + '"' + ":" + obj[key];
+			} else if (typeof obj[key] === "string" || typeof obj[key] === "number") {
+					JSONstring = '"' + key + '"' + ":" + '"' + obj[key] + '"';	
+			} else if (obj.hasOwnProperty('functions') || obj.hasOwnProperty('undefined')) {
+					return '{}';	
+			} else {
+					JSONstring = '"' + key + '"' + ":" + stringifyJSON(obj[key]);
 			}
+
+			result += JSONstring + ",";
+//					console.log("Result: ", result);
+		}
+
+		result = result.substring(0, result.length-1);
+//				console.log("Final Output: ", result);
+		return '{' + result + '}';
+
+	}
 };
