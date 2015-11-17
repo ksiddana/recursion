01f80355830dd37307416d2def086e41e84d8047
Karun Siddana <ksiddana@gmail.com>
2015-11-16 21:20:50 -0800

    Using recursion only

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,49 +5,10 @@
 
 // But instead we're going to implement it from scratch:
 
-//------------------------------------------------------------------
-/*var getElementsByClassName = function(className) {
-	
-  	console.log("---------------------");
-	console.log("ClassName:", className);
-	var expectedNodeList = [];
-	var childNodes = document.body.childNodes;
-	var nodes = document.body.children;
-	
-	if (document.body.className === className) {
-		expectedNodeList.push(document.body);
-	}
-  	 
-	for (var i = 0; i < nodes.length; i++) {
-//		console.log(nodes[i].classList[1]);	
-		  
-	  for (var j = 0; j < nodes[i].classList.length; j++) {
-			console.log(nodes[i].classList[j]);
-
-			if (nodes[i].className === className || nodes[i].classList[j] === className) {
-					expectedNodeList.push(nodes[i]);
-			}
-	  }
-	  	  
-	  if (nodes[i] !== null) {
-			console.log("This " + nodes[i] + " element has Child Nodes");
-			document.getElementsByClassName(className);
-	  }
-	}
-	console.log("Output: ", expectedNodeList, "Length: ", expectedNodeList.length);
-	return expectedNodeList;
-
-
-};*/
-
-//------------------------------------------------------------------
-
 var getElementsByClassName = function(className) {
   
   	console.log("----------- Using Recursion -------------");
 	var expectedNodeList = [];
-//  	var children = document.body.children;
-//    console.log(children);
 	
   	function recursiveCall(node) {
 	  if (node.className === className || node.classList.contains(className)) {
